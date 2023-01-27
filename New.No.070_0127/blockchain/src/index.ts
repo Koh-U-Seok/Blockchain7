61e9ad7daf57929dfb4a95e22b1c9d7886027a5c
Koh-U-Seok <kosh1668@naver.com>
2023-01-27 17:48:51 +0900

    2023-01-27 17:48 upload

---
+++ New.No.070_0127/blockchain/src/index.ts
@@ -0,0 +1,120 @@
+// import Block from "@core/block/block";
+// import Chain from "@core/chain";
+
+// const genesis = new Block(["첫 블록"]);
+// console.log("genesis : ", genesis);
+
+// const second = new Block(["두 번째 블록"], genesis);
+// console.log("second : ", second);
+
+// const previousBlock = new Block(["이전 블록"]);
+// previousBlock.height = 29;
+// previousBlock.difficulty = 10;
+// const adjustmentBlock = new Block(["단위 개수 전 블록"]);
+// adjustmentBlock.height = 20;
+// adjustmentBlock.difficulty = 11;
+
+// const newBlock = new Block(["asdf"], previousBlock, adjustmentBlock, {
+//   DAI: 10,
+//   averageGenerationTime: 60 * 1000,
+// });
+
+// // console.log(newBlock);
+
+// let chain = new Chain();
+
+// for (let i = 0; i < 300; i++) {
+//   chain.addBlock([`test block ${i}`]);
+// }
+// console.log("myChain : ", chain[0]);
+
+import P2P from "./p2p";
+import express, { Express, Request, Response } from "express";
+import { json } from "stream/consumers";
+
+const app: Express = express();
+const ws: P2P = new P2P();
+// P2P 클래스를 ws라는 이름으로 새로 선언한다. ws는 Chain 클래스를 상속받았다.
+
+app.use(express.json());
+
+// 체인 확인하는 용도
+// PostMan으로 주소는 http://localhost:8080/chains,
+// {
+//    "data" : "체인 데이터 요청"
+// }
+// 다른 사람과 주고 받는게 아니라 나 자신의 것을 확인할 용도이기 때문에 자신에게 보낸다.
+app.get("/chains", (req: Request, res: Response) => {
+  console.log(ws.getChain);
+  // get 메서드는 ()를 붙이지 않아도 된다.
+
+  res.json(ws.getChain);
+  // json()은 json의 형태로 데이터를 전송한다. res.send(JSON.stringify(ws.getChain))과 다르지 않다.
+  // PostMan에서 요청을 보냈으니 getChain으로 체인을 가져와서 json 형태로 대답한다.
+  // 객체를 보내면 터진다. 그렇기에 json으로 바꿔서 보낸다.
+});
+
+// 나 혼자 블록 생성하는 용도
+// PostMan으로 주소는 http://localhost:8080/block/mine,
+// {
+//    "data" : ["블록쌓기"]
+// }
+// 체인에 블록을 쌓는 용도가 아니라 일단 나혼자 블록을 쌓는 용도이기 때문에 자신에게 보낸다.
+//
+app.post("/block/mine", (req: Request, res: Response) => {
+  const { data }: { data: Array<string> } = req.body;
+  // req.body, 즉 {"data" :"블록쌓기"}를 data에 넣는다.
+  // {data}:{data:Array<string>}은 json을 객체의 형태로 넣겠다는 뜻이다.
+
+  const newBlock: IBlock | null = ws.addBlock(data);
+  // P2P가 상속받은 Chain의 addBlock에 객체화된 req.body인 data를 인자로 넣어 실행한다.
+  // IBlock | null 인 이유는 검사 용도의 메서드인 isValidBlock의 결과로 isError:true가 나오면 결과적으로 return값이 null이 나올 수 있기 때문이다.
+  // 성공적으로 나온다면 IBlock의 형태로 새로운 블럭이 newBlock에 정의될 것이다.
+  // 문제가 생겼다면 null의 형태로 newBlock이 정의될 것이다.
+
+  if (newBlock === null) res.send("error data");
+  // newBlock이 null이라면, 다시 말해 블록 생성에 실패했다면 "error data"를 전송한다.
+  // send()를 실행하면 통신은 종료되니 아래의 res.json()은 실행하지 않는다.
+
+  res.json(newBlock);
+  // if문에 걸리지 않았다면 성공적으로 제작된 블록을 json의 형태로 전송(대답)한다.
+});
+// 이후에 GET /chains 로 요청을 보내면 제네시스 블록과 새로 만든 블록이 같이 응답으로 오는 것을 확인할 수 있다.
+
+// 다른 사람의 체인과 비교해서(높이) 더 높은 것을 채택
+//                          상대 IP주소   포트번호
+// PostMan으로 주소는 http://192.168.0.227:8080/peer/add
+//            내 IP 주소
+// {
+//   "peer":"ws://192.168.0.227:7545"
+// }
+app.post("/peer/add", (req: Request, res: Response) => {
+  const { peer }: { peer: string } = req.body;
+  // req.body, 즉 {"peer":"ws://192.168.0.227:7545"}를 peer에 넣는다.
+  // {peer}:{peer:string}은 json의 형태로 온 req.body를 객체의 형태로 넣겠다는 뜻이다.
+  ws.addToPeer(peer);
+  // P2P 클래스로 선언된 ws의 addToPeer 메서드에 객체화한 req.body, 즉 {"peer":"ws://192.168.0.227:7545"}(보낸 사람의 IP. 내가 보내면 내 IP이며, 다른 사람이 나에게 통신을 시도하면 상대의 IP이다.)를 객체화 한것을 addToPeer 메서드의 인자로 담아 실행한다.
+  res.end();
+  // 통신을 종료한다.
+});
+
+// 나한테 접속한 다른 컴퓨터(== /peer/add를 했던 다른 컴퓨터)를 조회
+// 내가 다른 컴퓨터를 조회해도 해당 컴퓨터가 기록된다.
+app.get("/peer", (req: Request, res: Response) => {
+  const sockets = ws.getSocket.map(
+    (item: any) => item._socket.remoteAddress + ":" + item._socket.remotePort
+  );
+  res.json(sockets);
+});
+
+const ports = [
+  [8080, 7545],
+  [8081, 7546],
+];
+const idx = 0;
+
+app.listen(ports[idx][0], () => {
+  console.log("server start ", ports[idx][0]);
+  ws.listen(ports[idx][1]);
+  // WebSocket(P2P) 서버 생성/배포
+});
---
+++ New.No.070_0127/blockchain/src/p2p/index.ts
@@ -0,0 +1,154 @@
+// npm i ws express
+// npm i -D @types/ws @types/express
+
+import { WebSocket, WebSocketServer } from "ws";
+import Chain from "@core/chain";
+
+// const TYPE = {
+//   BLOCK: 1,
+//   CHAIN: 2,
+// };
+
+// TYPE.BLOCK
+
+enum MessageType {
+  // enum : 배열과 비슷하게 순서가 있는 데이터이다.
+  //    - 열거형 이라고 한다.
+  //    - 위의 코드와 같은 역할을 한다.
+  //    - 변수에 정의할 값을 미리 정의했다고 생각하자.
+  // MessageTYPE << 왜 정의했느냐? << 어떤 메세지를 주고 받았는지 확인하기 위해서 타입으로 설정했다. 혹시나 모를 오타와 같은 오류를 배제하기 위함이다.
+  lastBlock = 0,
+  // 마지막 블록을 달라고 하고 준다.
+  allBlock = 1,
+  // 전체 체인 달라고 하고 준다.
+  addBlock = 2,
+  // 블록이 추가되었다고 알려주고 뭐가 추가되었는지 알려준다.
+}
+// 오타같은 오류를 줄이기 위해서 사용한다.
+
+interface IMessage {
+  // 주고 받을 메세지에 대한 타입
+  type: MessageType;
+  // 어떤 메세지를 주고 받았는지 확인
+  payload: any;
+  // 메세지에 담긴 데이터
+}
+
+class P2P extends Chain {
+  //  Chain을 상속받는 이유 : 현재 P2P 서버에 기존의 체인을 상속함으로써 블록 추가 등에 있어서 편하다.
+
+  private socket: Array<WebSocket>; // 연결된 peer의 목록
+
+  constructor() {
+    super();
+    this.socket = [];
+  }
+
+  get getSocket(): Array<WebSocket> {
+    return [...this.socket];
+  }
+  connectSocket(socket: WebSocket, type?: MessageType): void {
+    // POST /peer/add 통신을 보낸 상대와 소켓을 연결하는 메서드다.
+    this.socket.push(socket);
+    // 연결된 소켓을 socket 배열에 추가한다. (peer 목록에 추가)
+    // socket은 상대방의 socket 서버 객체다. 저장하기 위한 배열인 this.socket 배열에 저장한다.
+    //    - 나중에 어디와 연결되었는지 확인할 때 사용한다.
+
+    socket.on("message", (_data: string) => {
+      // message 이벤트가 발생하면 로그로 남긴다.
+      console.log(_data.toString());
+
+      const data: IMessage = JSON.parse(_data.toString());
+      // 받은 메세지를 문자열로 변환시킨 다음 JSON.parse() 메서드를 사용해 객체로 변환한다.
+      // 변환되어 만들어진 객체는 IMessage의 형태로 data에 넣어진다.
+
+      switch (data.type) {
+        // 어떤 요청이 왔는가 type으로 확인해서
+        case MessageType.lastBlock: {
+          // 마지막 블록을 달라고 했으니
+          const message: IMessage = {
+            type: MessageType.allBlock,
+            payload: [this.lastBlock],
+            // 마지막 블록을 payload에 담아서
+          };
+          socket.send(JSON.stringify(message));
+          // 보낸다.
+          break;
+        }
+        case MessageType.allBlock: {
+          const [newBlock]: [IBlock] = data.payload;
+          const isValid: IBlock | null = this.add2Chain(newBlock);
+
+          if (isValid !== null) break;
+          //  isValid가 null이 아니다 >> 체인에 블록이 잘 들어갔다.
+
+          // 체인에 블록이 정상적으로 추가되지 않았을때 전체 체인을 보내서 확인한다.
+          const message: IMessage = {
+            type: MessageType.addBlock,
+            payload: this.getChain,
+          };
+          socket.send(JSON.stringify(message));
+          break;
+        }
+        case MessageType.addBlock:
+          const isValidChain = this.isValidChain(data.payload);
+          if (isValidChain.isError == true) break;
+
+          const isValid = this.replaceChain(data.payload);
+          if (isValid.isError == true) break;
+
+          // 나와 연결된 peer들에게 내가 데이터 바뀌었음을 알린다.
+          const message: IMessage = {
+            type: MessageType.addBlock,
+            payload: data.payload,
+          };
+
+          this.socket.forEach((item) => {
+            item.send(JSON.stringify(message));
+          });
+          break;
+      }
+    });
+
+    const message: IMessage = {
+      // 처음 연결 시 요청을 보내자. 마지막 블럭을 원한다고
+      type: type | MessageType.lastBlock,
+      payload: type ? this.getChain : [],
+    };
+
+    socket.send(JSON.stringify(message));
+    // 방금 연결한 Socket 서버에  message 이벤트를 보낸다.
+  }
+
+  listen(port: number): void {
+    // 현재 로컬에서 서버를 생성, 배포한다.
+    const server: WebSocketServer = new WebSocket.Server({ port });
+    // 서버를 생성한다.
+    /// 가나슈(Ganache)라는 개인용 블록체인이 있다. << 네트워크 없이 진행 가능하다.
+    // 이 가나슈의 초기 port 설정이 7545이다.
+
+    server.on("connection", (socket: WebSocket) => {
+      // 서버에 연결이 들어왔을 때
+      console.log("socket start");
+      this.connectSocket(socket);
+      //   socket을 추가한다.
+    });
+  }
+
+  addToPeer(peer: string): void {
+    // 소켓을 생성하고 연결한다.
+    const socket: WebSocket = new WebSocket(peer);
+    // 상대 소켓 서버 주소를 받아서 연결을 시도한다.
+    // peer에 들어있는 주소는 보낸 사람의 IP다.
+    // 내가 /peer/add로 Post 통신을 보내면 받는 서버의 입장에서 peer, 내 ip는 상대의 ip인 셈이다.
+    socket.on("open", () => {
+      // 상대의 socket 서버와 연결 성공 시 open 이벤트가 발생한다.
+      console.log("open");
+      this.connectSocket(socket, MessageType.addBlock);
+      // socket 서버와, 체인에 블록을 추가하기 위해 통신을 하였기에 MessageType.addBlock의 유형으로 이 클래스의 connectSocket 메서드를 실행한다.
+      // 연결에 성공하면 소켓에 추가한다.
+    });
+  }
+}
+
+export default P2P;
